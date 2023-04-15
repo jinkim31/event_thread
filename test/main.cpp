28e12c49772d2e0d990fa459e6175247afa7c853
jinkim31 <jinkim@kaist.ac.kr>
2023-04-15 20:35:57 +0900

    added test

---
+++ test/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "../event_thread/eventThread.h"
+
+using namespace ethr;
+
+class Object : public EThreadObject
+{
+public:
+    void hi(std::string name){std::cout<<"hi "<<name<<std::endl;}
+};
+int main()
+{
+    Object o;
+    EThread thread("test");
+    o.moveToThread(thread);
+    o.callQueued(&Object::hi, std::string("jin"));
+    thread.start();
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    thread.stop();
+}
