5e0d8d36885a75fabdb3c583454ab7f0fd8433ae
jinkim31 <jinkim@kaist.ac.kr>
2023-08-21 16:19:24 +0900

    added EPromiseMove

--- event_thread/epromise.h
+++ event_thread/epromise.h
@@ -122,6 +122,114 @@ private:
     std::function<void(std::exception_ptr)> mCatchFunctor;
     EDeletable *mThenPromisePtr;
 };
+
+template<typename PromiseType, typename... ParamTypes>
+class EPromiseMove : public EDeletable
+{
+public:
+    EPromiseMove(UntypedEObjectRef eObjectRef, const std::function<PromiseType(ParamTypes&&...)> functor)
+    {
+        if(!eObjectRef.isInitialized())
+            throw std::runtime_error("[EThread] EPromise is created using empty EObject reference.");
+        mTargetEObjectRef = eObjectRef;
+        mExecuteFunctor = functor;
+        mThenPromisePtr = nullptr;
+        mInitialized = true;
+    }
+
+    template<typename EObjectType>
+    EPromiseMove(EObjectRef<EObjectType> eObjectRef, PromiseType(EObjectType::*funcPtr)(ParamTypes&&...))
+            : EPromiseMove(eObjectRef, [=](ParamTypes&&... params){return ((*(eObjectRef.eObjectUnsafePtr())).*funcPtr)(std::move(params...));}){}
+
+    void selfDestructChain()
+    {
+        // TODO: destruct promises after exception catch
+        delete this;
+    }
+
+    void execute(ParamTypes&&... params)
+    {
+        if (!mInitialized)
+            return;
+
+        mTargetEObjectRef.runQueued([&, params = std::move(params...)]() mutable
+        {
+            try
+            {
+                if (mThenPromisePtr)
+                    mExecuteThenFunctor(std::move(mExecuteFunctor(std::move(params))));
+                else
+                    mExecuteFunctor(std::move(params));
+            }
+            catch (const std::exception& e)
+            {
+                if (!mCatchEObjectRef.isInitialized())
+                    throw ExceptionNotCaughtException(
+                            "[EThread] Detected uncaught exception: \"" + std::string(e.what())
+                            + "\". Use EPromise::cat() to catch the exception.");
+
+                std::exception_ptr eptr = std::current_exception();
+                mCatchEObjectRef.runQueued([&]{
+                    mCatchFunctor(eptr);
+                });
+            }
+
+            delete this;
+        });
+    }
+
+    template<typename EObjectType>
+    EPromiseMove<PromiseType, ParamTypes...>* cat(
+            EObjectRef<EObjectType> eObjectRef,
+            void(EObjectType::*funcPtr)(std::exception_ptr))
+    {
+        mCatchEObjectRef = eObjectRef;
+        mCatchFunctor = std::bind(funcPtr, eObjectRef.mUntypedEObjectUnsafePtr, std::placeholders::_1);
+        return this;
+    }
+
+    EPromiseMove<PromiseType, ParamTypes...>* cat(
+            UntypedEObjectRef eObjectRef,
+            const std::function<void(std::exception_ptr)>& functor)
+    {
+        mCatchEObjectRef = eObjectRef;
+        mCatchFunctor = functor;
+        return this;
+    }
+
+    template<typename ThenPromiseType, typename EObjectType>
+    EPromiseMove<ThenPromiseType, PromiseType>* then(
+            EObjectRef<EObjectType> eObjectRef,
+            ThenPromiseType(EObjectType::*funcPtr)(PromiseType&&))
+    {
+        auto thenPromise = new EPromiseMove<ThenPromiseType, PromiseType>(eObjectRef, funcPtr);
+        mExecuteThenFunctor = [=](PromiseType&& output){ thenPromise->execute(std::move(output)); };
+        mThenPromisePtr = thenPromise;
+        return thenPromise;
+    }
+
+    template<typename ThenPromiseType>
+    EPromiseMove<ThenPromiseType, PromiseType>* then(
+            UntypedEObjectRef eObjectRef,
+            const std::function<ThenPromiseType&&(PromiseType&&)>& functor)
+    {
+        auto thenPromise = new EPromiseMove<ThenPromiseType, PromiseType>(eObjectRef, functor);
+        mExecuteThenFunctor = [=](PromiseType&& output){ thenPromise->execute(std::move(output)); };
+        mThenPromisePtr = thenPromise;
+        return thenPromise;
+    }
+
+private:
+    bool mInitialized;
+    UntypedEObjectRef mTargetEObjectRef;
+    std::function<void(PromiseType&&)> mExecuteThenFunctor;
+    std::function<PromiseType&&(ParamTypes&&...)> mExecuteFunctor;
+    UntypedEObjectRef mCatchEObjectRef;
+    std::function<void(std::exception_ptr)> mCatchFunctor;
+    EDeletable *mThenPromisePtr;
+};
+
 }
+
 
 #endif
---
+++ test/promise_move/main.cpp
@@ -0,0 +1,84 @@
+#include <ethread.h>
+#include <etimer.h>
+#include <epromise.h>
+
+using namespace ethr;
+
+class PassTester
+{
+public:
+    explicit PassTester(const int& num){mNum = num; std::cout<<"PassTester("<<num<<") constructed"<<std::endl;}
+    PassTester(const PassTester& passTester){mNum = passTester.mNum; std::cout<<"PassTester("<<mNum<<") copied"<<std::endl;}
+    PassTester(PassTester&& passTester) noexcept {mNum = passTester.mNum; std::cout<<"PassTester("<<mNum<<") moved"<<std::endl;}
+    ~PassTester(){std::cout<<"PassTester("<<mNum<<") destructed"<<std::endl;}
+    int num() const{return mNum;}
+private:
+    int mNum;
+};
+
+class Worker : public EObject
+{
+public:
+    PassTester&& echo(PassTester&& tester)
+    {
+        std::cout<<"echo"<<std::endl;
+        return std::move(tester);
+    }
+    PassTester&& echo2(PassTester&& tester)
+    {
+        std::cout<<"echo2"<<std::endl;
+        return std::move(tester);
+    }
+};
+
+class App : public EObject
+{
+public:
+    App()
+    {
+        mWorker.moveToThread(mThread);
+        mTimer.moveToThread(EThread::mainThread());
+        mTimer.addTask(0, std::chrono::milliseconds(0), this->uref(), [&]
+        {
+            auto promise = new EPromiseMove(mWorker.ref<Worker>(), &Worker::echo);
+            promise
+            ->then(mWorker.ref<Worker>(), &Worker::echo2)
+            ->then(mWorker.ref<Worker>(), &Worker::echo)
+            ->then(mWorker.ref<Worker>(), &Worker::echo2)
+            ->then(mWorker.ref<Worker>(), &Worker::echo)
+            ->then(mWorker.ref<Worker>(), &Worker::echo2)
+            ->then<int>(this->uref(), [](PassTester&& tester){
+                std::cout<<"test done"<<std::endl;
+                return 0;});
+            PassTester tester(123);
+
+            promise->execute(std::move(tester));
+        }, 1);
+        mTimer.addTask(1, std::chrono::milliseconds(2000), this->uref(), []
+        {
+            EThread::stopMainThread();
+        }, 1);
+        mTimer.start();
+        mThread.start();
+    }
+    ~App()
+    {
+        mThread.stop();
+        mWorker.removeFromThread();
+        mTimer.removeFromThread();
+    }
+private:
+    EThread mThread;
+    Worker mWorker;
+    ETimer mTimer;
+};
+
+int main()
+{
+    EThread mainThread("main");
+    EThread::provideMainThread(mainThread);
+    App app;
+    app.moveToThread(mainThread);
+    mainThread.start();
+    app.removeFromThread();
+}
